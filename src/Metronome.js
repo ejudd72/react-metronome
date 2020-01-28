e8764f630e09a1f8965f3df75edc01db14bfc530
Eleanor <e.e.j@live.co.uk>
2020-01-28 20:34:00 +0000

    added sound files, can now play a metronome sound with varying clicks which dictate the first beat of each bar

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import click1 from './click1.wav';
+import click2 from './click2.wav';
 import './Metronome.css';
 
 class Metronome extends Component {
@@ -13,8 +15,46 @@ class Metronome extends Component {
             beatsPerBar: 4
         }
 
+        this.click1 = new Audio(click1);
+        this.click2 = new Audio(click2);
+
         this.handleBpmChange = this.handleBpmChange.bind(this);
     }
+
+    startStop = () => {
+        if (this.state.playing) {
+            clearInterval(this.timer);
+            this.setState({
+                playing: false
+            });
+        } else {
+            this.timer = setInterval(
+                this.playClick,
+                (60 / this.state.bpm) * 1000
+            );
+            this.setState(
+                {
+                    count: 0,
+                    playing: true
+                },
+                this.playClick
+            );
+        }
+    }
+
+    playClick = () => {
+        const { count, beatsPerBar } = this.state;
+
+        if (count % beatsPerBar === 0) {
+            this.click2.play();
+        } else {
+            this.click1.play();
+        }
+
+        this.setState(state => ({
+            count: (state.count + 1) % state.beatsPerBar
+        }));
+    };
 
     handleBpmChange = e => { 
         const bpm = e.currentTarget.value;
@@ -35,7 +75,7 @@ class Metronome extends Component {
                 value={bpm}
                 onChange={this.handleBpmChange} />
             </div>
-            <button>
+            <button onClick={this.startStop}>
               {playing ? 'Stop' : 'Start'}
             </button>
           </div>
