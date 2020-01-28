4b5df2572090b14db11b21cf535f00ca4fa74954
Eleanor <e.e.j@live.co.uk>
2020-01-28 20:07:30 +0000

    set up initial files, hello world message to start with

---
+++ src/Metronome.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Metronome.css';
+
+class Metronome extends Component {
+    render() {
+    return <div className="metronome">hello world!</div>;
+    } 
+}
+
+export default Metronome;
